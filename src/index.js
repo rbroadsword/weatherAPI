7e65479b4aeded4aa54742eb260e89b6f179c35b
Ryan Broadsword <rbroadsword@gmail.com>
2022-05-22 14:53:56 -0700

    api project outline

--- src/index.js
+++ src/index.js
@@ -5,6 +5,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 
-$(document).ready(function() {
+$(function() {
+  $('#weatherLocation').on("Get Current Temperature and Humidity", function() {
+    const city = $('#location').val(); 
+    $('#location').val(""); 
 
+    let request = new XMLHttpRequest(); 
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[YOUR-API-KEY-HERE]`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText); 
+        getElements(response); 
+      }
+    }; 
+    request.open("GET", url, true); 
+    request.send(); 
+
+    function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+  })
 }); 
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'template',      // 
+      title: 'weather',      // 
       template: './src/index.html',
       inject: 'body'
     })
